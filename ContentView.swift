5a3ef45cdc04a7b89a2448e7aa635f63e4c83be0
Keerthik Muruganandam <70237309+krthkmgndm@users.noreply.github.com>
2024-08-13 12:10:46 -0500

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,113 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Keerthik Muruganandam on 8/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAns = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    
+    @State private var gameOver = false
+    @State private var questionsAsked = 0
+    
+    var body: some View {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundStyle(.secondary)
+                        Text(countries[correctAns])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                .alert(scoreTitle, isPresented: $showingScore) {
+                    Button("Continue", action: askQuestion)
+                } message: {
+                    Text("Your score is \(score)")
+                }
+                
+                Spacer()
+                Spacer()
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
+            }
+            .padding()
+        }
+        .alert("Game Over", isPresented: $gameOver) {
+            Button("New Game", action: newGame)
+        } message: {
+            Text("You got \(score)/8")
+        }
+        
+        
+    }
+    
+    func flagTapped(_ number: Int) {
+        questionsAsked += 1
+        if number == correctAns {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            score -= 1
+        }
+        
+        showingScore = true
+        if questionsAsked == 8 {
+            gameOver = true
+        }
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAns = Int.random(in: 0...2)
+    }
+    
+    func newGame() {
+        questionsAsked = 0
+        score = 0
+        askQuestion()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Keerthik Muruganandam on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
